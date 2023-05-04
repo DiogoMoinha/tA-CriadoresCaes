90ef9b0e192dd84862ac926739436c892c5bad77
Diogo Moinhá <aluno23040@ipt.pt>
2023-05-04 13:06:10 +0100

    adição de restrições de tamanho(stringLength) e valor (regularExpression) aos Criadores

--- CriadoresCaes/CriadoresCaes/Models/Criadores.cs
+++ CriadoresCaes/CriadoresCaes/Models/Criadores.cs
@@ -1,4 +1,6 @@
-﻿namespace CriadoresCaes.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CriadoresCaes.Models
 {
     /// <summary>
     /// Dados dos criadores dos animais
@@ -14,36 +16,51 @@
             ListaRacas = new HashSet<Racas>();
         }
 
+
         public int Id { get; set; }
 
         /// <summary>
         /// Nome do criador
         /// </summary>
+        [Required(ErrorMessage = "O {0} é de preenchimento obrigatório.")]
+        [StringLength(50)]
         public string Nome { get; set; }
 
         /// <summary>
         /// nome pelo qual o criador é conhecido no mercado de venda de animais
         /// </summary>
+        [Display(Name = "Nome Comercial")]
+        [StringLength(50)]
         public string NomeComercial { get; set; }
 
         /// <summary>
         /// morada do criador
         /// </summary>
+        [StringLength(100)]
         public string Morada { get; set; }
 
         /// <summary>
         /// Código Postal
         /// </summary>
+        [Display(Name = "Código Postal")]
+        [RegularExpression("[1-9][0-9]{3}-[0-9]{3}( ){1,3}[A-Z -ÇÀÁÉÍÓÚÂÊÎÔÛÃÕ]+", 
+            ErrorMessage = "O {0} deve ter o formato XXXX-XXX NOME DA TERRA")]
         public string CodPostal { get; set; }
 
         /// <summary>
         /// Email do criador
         /// </summary>
+        [EmailAddress]
+        [Required(ErrorMessage = "O {0} é de preenchimento obrigatório.")]
         public string Email { get; set; }
 
         /// <summary>
         /// Telemóvel do Criador
         /// </summary>
+        [Required(ErrorMessage = "O {0} é de preenchimento obrigatório.")]
+        [Display(Name = "Telemóvel")]
+        [StringLength(9, MinimumLength = 9, ErrorMessage = "Deve escrever {1} caracters no número de {0}")]
+        [RegularExpression("9[1236][0 - 9]{7}")] 
         public string Telemovel { get; set; }
 
 
